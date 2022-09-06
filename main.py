5e3b65e2928c823f87c1a460dc0ecc4f8a449f28
lissaneeddine <57492062+lissaneeddine@users.noreply.github.com>
2022-09-06 22:16:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import time
 import requests
 from datetime import datetime
 
+def sendmessage()
+    message = "heloooooooooooooo"
+    bot_token  = "5519021114:AAHKSxETRcHWdDY8De-2HiqZX27NlZ0GSiw"
+    bot_chat_id = "-1001646447755"
+    send_text = "https://api.telegram.org/bot"+bot_token+"/sendmessage?chat_id="+bot_chat_id+"&parse_mode=Markdown&text="+message
+
+    response = requests.get(send_text)
+    return response
 
 intervals = [ "00" , "05" , "10" , "15" , "20" , "25" , "30" , "35" , "40" , "45" , "50" , "55" ]
 
@@ -12,12 +20,8 @@ while True:
 
         if time_min in intervals and  time_min != last_min_check:
             last_min_check = time_min
-            message = "heloooooooooooooo"
-            bot_token  = "5519021114:AAHKSxETRcHWdDY8De-2HiqZX27NlZ0GSiw"
-            bot_chat_id = "-1001646447755"
-            send_text = "https://api.telegram.org/bot"+bot_token+"/sendmessage?chat_id="+bot_chat_id+"&parse_mode=Markdown&text="+message
-
-            response = requests.get(send_text)
+            sendmessage()
+    
     
 
 
